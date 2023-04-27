bbc2722d7642bdb7f1205e6a6d62a61fc98be937
Stanisław Fiedoruk <11120@kasprzak.edu.pl>
2023-04-27 10:52:18 +0200

    bez php

---
+++ biuro.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Wycieczki Krajoznawcze</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+
+<body>
+    <header>
+        <h1>WITAMY W BIURZE PODRÓŻY</h1>
+    </header>
+    <div id="top">
+        <h3>ARCHIWUM WYCIECZEK</h3>
+        <?php
+        // S1
+        ?>
+    </div>
+    <div id="left">
+        <h3>NAJTANIEJ</h3>
+        <table>
+            <tr>
+                <td>Włochy</td>
+                <td>od 1200 zł</td>
+            </tr>
+            <tr>
+                <td>Francja</td>
+                <td>od 1200 zł</td>
+            </tr>
+            <tr>
+                <td>Hiszpania</td>
+                <td>od 1400 zł</td>
+            </tr>
+        </table>
+    </div>
+    <div id="mid">
+        <h3>TU BYLIŚMY</h3>
+        <?php
+        // s2
+        ?>
+    </div>
+    <div id="right">
+        <h3>SKONTAKTUJ SIĘ</h3>
+        <a href="wycieczki@wycieczki.pl">napisz do nas</a>
+        <p>telefon 555666777</p>
+    </div>
+    <footer>
+        <p>Stronę wykonał: Stanisław Fiedoruk 4J </p>
+    </footer>
+</body>
+
+</html>
